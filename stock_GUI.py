c9c76712fa2822e6614381c468b9fa46bbb40ae8
wsdonald4 <93047675+wsdonald4@users.noreply.github.com>
2021-10-23 11:50:18 -0400

    Add files via upload

---
+++ stock_GUI.py
@@ -0,0 +1,171 @@
+# Summary: This module contains the user interface and logic for a graphical user interface version of the stock manager program.
+# Author: 
+# Date: 
+
+from datetime import datetime
+from stock_class import Stock, DailyData
+from os import path
+from tkinter import *
+from tkinter import ttk
+from tkinter import messagebox, simpledialog, filedialog
+import csv
+import matplotlib.pyplot as plt
+import json
+
+
+class StockApp:
+    def __init__(self):
+        self.stock_list = []
+
+        
+        
+        # Create Window
+        self.root = Tk()
+        self.root.title("Stock Manager")
+      
+        # Add Menu
+        self.menubar = Menu(self.root)
+
+        self.filemenu = Menu(self.menubar, tearoff=0)
+
+        
+        self.webmenu = Menu(self.menubar, tearoff=0)
+        self.webmenu.add_command(label = "Import CSV from Yahoo! Finance...", command=self.importCSV_web_data)
+        self.menubar.add_cascade(label="Web",menu=self.webmenu)
+
+        self.chartmenu = Menu(self.menubar,tearoff=0)
+        self.chartmenu.add_command(label="Display Stock Chart", command=self.display_chart)
+        self.menubar.add_cascade(label="Chart",menu=self.chartmenu)
+
+
+        self.root.config(menu=self.menubar)
+
+        # Add heading information
+        self.headingLabel = Label(self.root,text="No Stock Selected")
+        self.headingLabel.grid(column=0,row=0,columnspan=3,padx = 5, pady = 10)
+        
+
+        # Add stock list
+        self.stockLabel = Label(self.root,text="Stocks")
+        self.stockLabel.grid(column=0,row=1,padx = 5, pady = 10,sticky=(N))
+
+        self.stockList = Listbox(self.root)
+        self.stockList.grid(column=0,row=2,padx = 5, pady = 5,sticky=(N,S))
+        self.stockList.bind('<<ListboxSelect>>',self.update_data)
+        
+        
+        # Add Tabs
+        self.notebook = ttk.Notebook(self.root,padding="5 5 5 5")
+       
+        self.notebook.grid(column=2,row=2,sticky=(N,W,E,S))
+        self.mainFrame = ttk.Frame(self.notebook)
+        self.stockDataFrame = ttk.Frame(self.notebook)
+        self.reportFrame = ttk.Frame(self.notebook)
+        self.chartFrame = ttk.Frame(self.notebook)
+        self.notebook.add(self.mainFrame,text='Manage')
+        self.notebook.add(self.stockDataFrame,text='History')
+        self.notebook.add(self.reportFrame,text = 'Report')
+        
+
+        # Set Up Main Tab
+        self.addStockGroup = LabelFrame(self.mainFrame,text="Add Stock",padx=5,pady=5)
+        self.addStockGroup.grid(column=0,row=0,padx=5,pady=5,sticky=(W,E))
+     
+        self.addSymbolLabel = Label(self.addStockGroup,text = "Symbol")
+        self.addSymbolLabel.grid(column=0,row=0,padx = 5, pady = 5,sticky=(W))
+        self.addSymbolEntry = Entry(self.addStockGroup)
+        self.addSymbolEntry.grid(column=1,row=0,padx=5,pady=5)
+
+        self.addNameLabel = Label(self.addStockGroup,text = "Name")
+        self.addNameLabel.grid(column=0,row=1,padx = 5, pady = 5,sticky=(W))
+        self.addNameEntry = Entry(self.addStockGroup)
+        self.addNameEntry.grid(column=1,row=1,padx=5,pady=5)
+
+        self.addSharesLabel = Label(self.addStockGroup,text = "Shares")
+        self.addSharesLabel.grid(column=0,row=2,padx = 5, pady = 5,sticky=(W))
+        self.addSharesEntry = Entry(self.addStockGroup)
+        self.addSharesEntry.grid(column=1,row=2,padx=5,pady=5)
+
+        self.addStockButton = Button(self.addStockGroup,text = "New Stock",command=self.add_stock)
+        self.addStockButton.grid(column=0,row=3,columnspan = 2, padx = 5, pady = 5)
+
+
+        self.deleteGroup = LabelFrame(self.mainFrame,text="Delete Stock",padx=5,pady=5)
+        self.deleteGroup.grid(column=0,row=2,padx=5,pady=5,sticky=(W,E))
+
+        self.deleteStockButton = Button(self.deleteGroup,text="Delete Selected Stock",command=self.delete_stock)
+        self.deleteStockButton.grid(column=0,row=0,padx=5,pady=5)
+
+
+
+        # Setup History Tab
+        self.dailyDataList = Text(self.stockDataFrame,width=40)
+        self.dailyDataList.grid(column=0,row=0,padx = 5, pady = 5)
+        
+        
+
+
+        # Setup Report Tab
+        self.stockReport = Text(self.reportFrame,width=40)
+        self.stockReport.grid(column=0,row=0,padx=5,pady=5)
+
+        self.root.mainloop()
+
+ 
+
+    # Refresh history and report tabs
+    def update_data(self, evt):
+        self.display_stock_data()
+
+    # Display stock price and volume history.
+    def display_stock_data(self):
+        messagebox.showinfo("This module is under construction") 
+    
+    # Add new stock to track.
+    def add_stock(self):
+        messagebox.showinfo("This module is under construction") 
+
+
+    # Remove stock and all history from being tracked.
+    def delete_stock(self):
+        messagebox.showinfo("This module is under construction") 
+
+
+
+      # Get price and volume history from Yahoo! Finance using CSV import.
+    def import_stock_csv(self,stock_list,symbol,filename):
+        for stock in stock_list:
+                if stock.symbol == symbol:
+                    with open(filename, newline='') as stockdata:
+                        datareader = csv.reader(stockdata,delimiter=',')
+                        next(datareader)
+                        for row in datareader:
+                            daily_data = DailyData(str(row[0]),float(row[4]),float(row[6]))
+                            stock.add_data(daily_data)   
+
+    # Import CSV stock history file.
+    def importCSV_web_data(self):
+        filename = filedialog.askopenfilename(title="Select " + symbol + " File to Import",filetypes=[('Yahoo Finance! CSV','*.csv')])
+        symbol = self.stockList.get(self.stockList.curselection())
+        if filename != "":
+            self.import_stock_csv(self.stock_list,symbol,filename)
+            self.display_stock_data()
+            messagebox.showinfo("Import Complete",symbol + "Import Complete")   
+    
+    # Display stock price chart.
+    def display_chart(self):
+        messagebox.showinfo("This module is under construction") 
+
+
+
+        
+    
+    
+
+def main():
+        app = StockApp()
+        
+
+if __name__ == "__main__":
+    # execute only if run as a script
+    main()
